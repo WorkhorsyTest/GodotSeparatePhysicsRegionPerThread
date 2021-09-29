f267460e2a5060ce2e57f8fefe34564ba2bd0899
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-09-28 21:12:51 -0700

    Made 2 difference scenes run in own viewports

--- src/Game/Game.gd
+++ src/Game/Game.gd
@@ -1,5 +1,6 @@
 extends Spatial
 
+export var ball_multiplier = 5.0
 onready var _ball_instancer := preload("res://src/Ball/Ball.tscn")
 var _rng : RandomNumberGenerator
 var _ball_count := 0
@@ -11,7 +12,7 @@ func _ready() -> void:
 
 func _physics_process(delta):
 	if Input.is_action_pressed("Jump"):
-		for n in 5:
+		for n in ball_multiplier:
 			var ball = _ball_instancer.instance()
 			self.add_child(ball)
 			ball.transform.origin.y = _rng.randf_range(5.0, 15.0)
@@ -23,4 +24,4 @@ func _physics_process(delta):
 func _on_FPSTimer_timeout():
 	var fps = Engine.get_frames_per_second()
 	var title = "FPS: %s | Count: %s" % [fps, _ball_count]
-	OS.set_window_title(title)
+	$FPS.text = title
